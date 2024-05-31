3dd0b10abaf5ed031836e000b3cac741a85bda8c
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-05-31 23:48:23 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        niramit: "'Niramit', sans-serif",  // Adds a new `font-display` class
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
